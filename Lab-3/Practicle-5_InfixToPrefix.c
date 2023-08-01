25d30d96c01c2416f656df5dd57c50d38a3e32d0
Krishna <119056745+KrishnaBalasara@users.noreply.github.com>
2023-08-01 18:35:51 +0530

    Create Practicle-5_InfixToPrefix.c

---
+++ Lab-3/Practicle-5_InfixToPrefix.c
@@ -0,0 +1,105 @@
+// Write a C Program to implement Infix to prefix conversion using stack.
+
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include <stdlib.h>
+#define MAX 20
+void push(int);
+char pop();
+void infix_to_prefix();
+int precedence (char);
+char stack[20],infix[20],prefix[20];
+int top = -1;
+int main() {
+ printf("\nINPUT THE INFIX EXPRESSION : ");
+ scanf("%s",infix);
+ infix_to_prefix();
+ return 0;
+}
+void push(int pos) {
+ if(top == MAX-1) {
+ printf("\nOVERFLOW\n");
+ }
+ else {
+ top++;
+ stack[top] = infix[pos];
+ }
+}
+char pop() {
+ char ch;
+if(top < 0) {
+ printf("\nSTACK UNDERFLOW\n");
+ }
+ else {
+ ch = stack[top];
+ top--;
+ return(ch);
+ }
+ return 0;
+}
+void infix_to_prefix() {
+ int i = 0,j = 0;
+ strrev(infix);
+ while(infix[i] != '\0') {
+ if(infix[i] >= 'a' && infix[i] <= 'z') {
+ prefix[j] = infix[i];
+ j++;
+ i++;
+ }
+ else if(infix[i] == ')') {
+ push(i);
+ i++;
+ }
+ else if(infix[i] == '(') {
+ while(stack[top] != ')') {
+ prefix[j] = pop();
+ j++;
+ }
+ pop();
+i++;
+ }
+ else {
+ if(top == -1) {
+ push(i);
+ i++;
+ }
+ else if( precedence(infix[i]) < precedence(stack[top])) {
+ prefix[j] = pop();
+ j++;
+ while(precedence(stack[top]) > precedence(infix[i])) {
+ prefix[j] = pop();
+ j++;
+ if(top < 0) {
+ break;
+ }
+ }
+ push(i);
+ i++;
+ }
+ else if(precedence(infix[i]) >= precedence(stack[top])) {
+ push(i);
+ i++;
+ }
+ }
+ }
+ while(top != -1) {
+ prefix[j] = pop();
+ j++;
+ }
+ strrev(prefix);
+ printf("EQUIVALENT PREFIX NOTATION : %s ",prefix);
+}
+int precedence(char alpha) {
+ if(alpha == '+' || alpha =='-') {
+ return(1);
+ }
+ if(alpha == '*' || alpha =='/') {
+ return(2);
+ }
+ return 0;
+}
+
+// Output
+// INPUT THE INFIX EXPRESSION : a+b*c/d-e*f
+// EQUIVALENT PREFIX NOTATION : -+a/*bcd*ef
